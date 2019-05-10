7786a0b619817c31d8af43464b1f2561f098f15a
Matuz Bence <matuzb97@gmail.com>
2019-05-10 11:32:12 +0200

    admin

---
+++ src/main/java/com/tudo/todu/passw.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tudo.todu;
+
+/**
+ *
+ * @author g8matben
+ */
+public class passw {
+    
+    private String adminlog = "Admin";
+    
+    
+public  boolean  password(String a, String b){
+
+if(a == adminlog && b == adminlog){
+
+
+    return true;
+    
+}else{
+return false;
+}
+
+}
+    
+    
+    
+    
+}
